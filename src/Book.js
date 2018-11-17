6bf3e4014c79186b1326c2e80c343842f49d988b
Dane Hammer <dane.molotok@gmail.com>
2018-11-17 10:47:36 -0600

    add: Book component

---
+++ src/Book.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Book extends Component {
+  render() {
+    return (
+      <div>{this.props.book.title}</div>
+    )
+  }
+}
+
+export default Book
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Book from './Book'
 
 function Shelf(props) {
   return (
     <div className="shelf">
       <h2>{props.shelf.name}</h2>
       {props.shelf.books.map(book => (
-        <div><span>{book.title}</span></div>
+        <Book book={book} />
       ))}
     </div>
   )
