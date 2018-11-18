90fed02170cf4c50337dcb8fa038b48f176b0407
Dane Hammer <dane.molotok@gmail.com>
2018-11-18 11:22:53 -0600

    improve: sync search results shelves to known state

--- src/App.js
+++ src/App.js
@@ -6,37 +6,8 @@ import BookSearch from './BookSearch'
 
 class App extends Component {
   state = {
-    books: [],
-    searchResults: []
-  }
-
-  updateBooks() {
-    BooksAPI.getAll().then(books => {
-      this.setState({books})
-    })
-  }
-
-  componentDidMount() {
-    this.updateBooks()
-  }
-
-  handleShelfChange = (book, shelf) => {
-    BooksAPI.update(book, shelf).then(this.updateBooks())
-  }
-
-  handleSearchChange = (e) => {
-    e.preventDefault()
-    BooksAPI.search(e.target.value).then((books) => {
-      // search API is buggy and returns `{"books": {"error": ...}}
-      // breaking the data structure
-      const searchResults = books instanceof Array ? books : []
-      this.setState({searchResults})
-    })
-  }
-
-  render() {
-    const {books, searchResults} = this.state
-    const shelves = [
+    searchResults: [],
+    shelves: [
       {
         id: 'currentlyReading',
         name: 'Currently Reading',
@@ -53,9 +24,55 @@ class App extends Component {
         books: []
       }
     ]
-    shelves.map(shelf => {
-      shelf.books = books.filter(book => book.shelf === shelf.id)
+  }
+
+  updateBooks() {
+    const {shelves} = this.state
+
+    BooksAPI.getAll().then(books => {
+      shelves.map(shelf => {
+        shelf.books = books.filter(book => book.shelf === shelf.id)
+      })
+      this.setState({shelves})
     })
+  }
+
+  componentDidMount() {
+    this.updateBooks()
+  }
+
+  handleShelfChange = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(this.updateBooks())
+  }
+
+  handleSearchChange = (e) => {
+    e.preventDefault()
+
+    const {shelves} = this.state
+
+    BooksAPI.search(e.target.value).then((books) => {
+      // search API is buggy and returns `{"books": {"error": ...}}
+      // breaking the data structure
+      const searchResults = books instanceof Array ? books : []
+
+      searchResults.map(searchBook => {
+        let searchShelf
+        shelves.map(shelf => {
+          shelf.books.map(book => {
+            if (book.id === searchBook.id) {
+              searchShelf = shelf.id
+            }
+          })
+        })
+        searchBook.shelf = searchShelf
+      })
+
+      this.setState({searchResults})
+    })
+  }
+
+  render() {
+    const {shelves, searchResults} = this.state
 
     return (
       <div className='App'>
