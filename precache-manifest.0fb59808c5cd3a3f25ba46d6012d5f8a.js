3bf0a7ed612eda3f6e897577218d05003a99994f
Dane Hammer <dane.molotok@gmail.com>
2018-12-14 14:04:00 -0600

    Updates

---
+++ precache-manifest.0fb59808c5cd3a3f25ba46d6012d5f8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e97b10d304014acbba24eea844dc94eb",
+    "url": "/reactnd-myreads/static/media/caret_down.e97b10d3.svg"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/reactnd-myreads/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "bfbe6841f1998515757aa887e45088b8",
+    "url": "/reactnd-myreads/static/media/add.bfbe6841.svg"
+  },
+  {
+    "revision": "f2ac2238918c239766c2",
+    "url": "/reactnd-myreads/static/js/runtime~main.f2ac2238.js"
+  },
+  {
+    "revision": "11d29679a38716de6959",
+    "url": "/reactnd-myreads/static/js/main.11d29679.chunk.js"
+  },
+  {
+    "revision": "9808fc465cbfeb516e36",
+    "url": "/reactnd-myreads/static/js/1.9808fc46.chunk.js"
+  },
+  {
+    "revision": "11d29679a38716de6959",
+    "url": "/reactnd-myreads/static/css/main.18318049.chunk.css"
+  },
+  {
+    "revision": "79f55cf289624224e5e9e3952683030f",
+    "url": "/reactnd-myreads/index.html"
+  }
+];
