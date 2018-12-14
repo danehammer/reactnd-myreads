dd4c63aadbd4875c27dcecf831e80d9bebb44815
Dane Hammer <dane.molotok@gmail.com>
2018-12-14 09:55:05 -0600

    improve: add catch all 404 page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Route, Link} from 'react-router-dom'
+import {Route, Link, Switch} from 'react-router-dom'
 import * as BooksAPI from './utils/BooksAPI'
 import Shelf from './Shelf'
 import BookSearch from './BookSearch'
@@ -76,33 +76,41 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <Route exact path='/' render={() => (
-          <div>
-            <header className='App-header'>¡Libros Míos!</header>
-            {
-              shelves.map(shelf => (
-                <div className='shelf' key={shelf.id}>
-                  <h2>{shelf.name}</h2>
-                  <Shelf
-                    books={shelf.books}
-                    onShelfChange={this.handleShelfChange}
-                  />
-                </div>
-              ))
-            }
-            <Link
-              to='/search'
-              className='add-button'
-            >Add Book</Link>
-          </div>
-        )} />
-        <Route path='/search' render={() => (
-          <BookSearch
-            searchResults={searchResults}
-            onSearchChange={this.handleSearchChange}
-            onShelfChange={this.handleShelfChange}
-          />
-        )} />
+        <Switch>
+          <Route exact path='/' render={() => (
+            <div>
+              <header className='App-header'>¡Libros Míos!</header>
+              {
+                shelves.map(shelf => (
+                  <div className='shelf' key={shelf.id}>
+                    <h2>{shelf.name}</h2>
+                    <Shelf
+                      books={shelf.books}
+                      onShelfChange={this.handleShelfChange}
+                    />
+                  </div>
+                ))
+              }
+              <Link
+                to='/search'
+                className='add-button'
+              >Add Book</Link>
+            </div>
+          )} />
+          <Route path='/search' render={() => (
+            <BookSearch
+              searchResults={searchResults}
+              onSearchChange={this.handleSearchChange}
+              onShelfChange={this.handleShelfChange}
+            />
+          )} />
+          <Route render={() => (
+            <div>
+              <h3>Sorry, that page doesn't exist.</h3>
+              <Link to='/'>Click here to go to the main page</Link>
+            </div>
+          )} />
+        </Switch>
       </div>
     )
   }
