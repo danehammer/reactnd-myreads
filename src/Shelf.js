80b812a9b5c91d7fd95df22da90e4aa227804ccd
Dane Hammer <dane.molotok@gmail.com>
2018-12-14 13:36:10 -0600

    improve: document prop types on Shelf

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import Book from './Book'
+import PropTypes from 'prop-types'
 
 function Shelf(props) {
+  const {books, onShelfChange} = props
   return (
     <div className='book-list'>
-      {props.books.map(book => (
+      {books.map(book => (
         <Book
           book={book}
           key={book.id}
-          onShelfChange={props.onShelfChange}
+          onShelfChange={onShelfChange}
         />
       ))}
     </div>
   )
+}
+
+Shelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  onShelfChange: PropTypes.func.isRequired
 }
 
 export default Shelf
